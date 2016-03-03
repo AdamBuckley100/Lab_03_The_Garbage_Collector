9e127188c277cf7584d1d08b094659aa29269bbd
AdamBuckly <adambuckley@live.com>
2016-03-03 09:19:38 +0000

    save point

--- src/lab03/MainClassWorkings.java
+++ src/lab03/MainClassWorkings.java
@@ -7,43 +7,19 @@ import java.util.List;
 
 public class MainClassWorkings {
 	
-	// Global Variables are as follows:
-	// declare the three separate integer arrays.
-	List<Integer> redFish;
-	List<Integer> blueFish;
-	List<Integer> yellowFish;
+	RedFish redfish; // so redFish is 12 bytes?
+	BlueFish bluefish; // so blueFish is 8 bytes?
+	YellowFish yellowfish; // so yellowFish is 4 bytes cause it has reference? (global variable)
 	
+	// in the handle pool every entry same size
+	Object[] handlePool;
+	// Below: each and every memory block has a 4 byte header (an int if 4 bytes)
+	int[] objectPool;
 	/**
 	 * Constructor. 
 	 */
 	public MainClassWorkings()
 	{
-		// Initialize the three integer arrays.
-		List<Integer> redFish = new ArrayList<Integer>();
-		List<Integer> blueFish = new ArrayList<Integer>();
-		List<Integer> yellowFish = new ArrayList<Integer>();
 		
-		// 1 single integer is exactly 4 bytes.
-		// therefore if i add exactly 3 integer variables to the
-		// redFish integer array then the redFish integer array will be 
-		// exactly 12 bytes.
-		redFish.add(1);
-		redFish.add(2);
-		redFish.add(3);
-		
-		// 1 single integer is exactly 4 bytes.
-		// therefore if i add exactly 2 integer variables to the
-		// blueFish integer array then the blueFish integer array will be 
-		// exactly 8 bytes.
-		blueFish.add(1);
-		blueFish.add(2);
-		
-		// 1 single integer is exactly 4 bytes.
-		// therefore if i add exactly 1 integer variables to the
-		// yellowFish integer array then the yellowFish integer array will be 
-		// exactly 4 bytes.
-		yellowFish.add(3);
-		
-		// so after this redFish is 12 bytes, blueFish is 8 bytes and yellowFish is 4 bytes.
 	}
 }
